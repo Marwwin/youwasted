04431f475ae1c121c53b1f012a759f462328f222
Marwwin <markus.kankkonen@anvianet.fi>
2021-07-29 08:42:08 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,51 @@
+const startBtn = document.querySelector("#start")
+const clearBtn = document.querySelector("#clear")
+clearBtn.style.display = "none"
+let timer = null
+let seconds = 0
+
+startBtn.addEventListener("click", () => {
+  startTimer()
+})
+
+const startTimer = () => {
+  if (!timer) {
+    updateTime()
+    timer = setInterval(() => {
+      seconds++
+      updateTime()
+    }, 1000)
+    startBtn.innerHTML = "Stop"
+    startBtn.id = "stop"
+  }
+  else {
+    clearInterval(timer)
+    timer = null
+    startBtn.innerHTML = "Start"
+    startBtn.id = "start"
+    clearBtn.style.display = "block"
+  }
+}
+
+const updateTime = () => {
+  const people = parseInt(document.querySelector("#people").value)
+  const salary = parseInt(document.querySelector("#salary").value)
+  const resultMoney = document.querySelector("#result_money")
+  const resultTime = document.querySelector("#result_time")
+  let minutes = Math.floor(seconds / 60)
+  minutes = minutes.toString().length === 1 ? "0" + minutes : minutes
+  let secondsText = seconds % 60
+  secondsText = secondsText.toString().length === 1 ? "0" + secondsText : secondsText
+  resultMoney.innerHTML = (people * salary * seconds / 3600).toFixed(2) + "€"
+  resultTime.innerHTML = minutes + ":" + secondsText + " Minutes"
+}
+
+clearBtn.addEventListener("click", () => {
+  seconds = 0
+  const resultMoney = document.querySelector("#result_money")
+  const resultTime = document.querySelector("#result_time")
+  resultMoney.innerHTML = "0.00€"
+  resultTime.innerHTML = "00:00 Minutes"
+
+  clearBtn.style.display = "none"
+})
